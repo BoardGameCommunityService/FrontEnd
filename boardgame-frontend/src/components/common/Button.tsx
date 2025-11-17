2c7d4ca5941802667a453624a84817457d324d88
gimeast <slasnrndu@gmail.com>
2025-11-17 20:24:05 +0900

    feat: 회원가입(회원정보) 페이지 마크업

---
+++ boardgame-frontend/src/components/common/Button.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+interface ButtonType {
+  type: "button" | "submit";
+  text: string;
+  textSize?: string;
+  py?: string;
+  bgColor?: string;
+  textColor?: string;
+  icon?: React.ReactNode;
+  gap?: string;
+  handleSubmit?: () => void;
+  onClick?: () => void;
+}
+
+export default function Button({
+  type,
+  text,
+  textSize = "16px",
+  py = "py-4",
+  bgColor = "#EEF0F7",
+  textColor = "#767676",
+  icon,
+  gap = "gap-[4px]",
+  onClick,
+}: ButtonType) {
+  return (
+    <button
+      type={type}
+      className={`flex justify-center ${gap} rounded-lg bg-[${bgColor}] ${py} font-semibold text-[${textSize}] ${textColor} cursor-pointer`}
+      onClick={onClick}
+    >
+      {icon}
+      {text}
+    </button>
+  );
+}
