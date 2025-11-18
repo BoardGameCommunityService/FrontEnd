0247c3ef8ef0ed914d2d29eab3959958c1a373a7
jiunlee19 <tearofglass@naver.com>
2025-11-18 12:13:04 +0900

    feat/활동지역 입력 페이지 - Button, TextInput 컴포넌트 적용

--- boardgame-frontend/src/app/signup/location/page.tsx
+++ boardgame-frontend/src/app/signup/location/page.tsx
@@ -1,27 +1,28 @@
+import Button from "@/components/common/Button";
+import TextInput from '@/components/common/TextInput';
 import Image from "next/image";
 import Link from "next/link";
-import TextInput from '../components/TextInput';
 export default function Signup() {
   return (
-    <div className="mt-[84px] mx-[20px]">
+    <>
         <Link href="/signup">
           <Image src="/icons/ic_back.svg" alt="뒤로가기 버튼" width={24} height={24} />
         </Link>
         <h2 className="mt-4 font-semibold text-2xl text-[#161616]">활동 지역을 선택해주세요</h2>
 
-      <form action="/signup/location">
+      <form action="/signup/location" className="flex flex-col">
         <TextInput label="활동지역" name="userLocation" placeholder="지역구를 입력해주세요.(ex.강남구, 서초구)" isHidden={true}  />
-        <button type="submit" className="mt-6 w-full p-3 bg-[#161616] text-white rounded-xl text-sm font-medium">검색</button>
+        <Button type="submit" text="현재 위치로 찾기" btnSize="medium" bgColor="bg-[#06E393]" icon={<Image src="/icons/ic_gps.svg" alt="버튼" width={20} height={20} />} textColor="text-black"/>
       </form>
 
       <div className='mt-6 text-[#999999]'>
         <h3 className='text-xs'>검색 결과</h3>
         <ul>
           <li className='my-3 text-[#161616]'>
-            <button>서울특별시 강남구</button>
+            <button type="button">서울특별시 강남구</button>
           </li>
         </ul>
       </div>
-    </div>
+    </>
   );
 }
