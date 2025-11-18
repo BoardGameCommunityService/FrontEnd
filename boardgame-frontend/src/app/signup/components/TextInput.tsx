af8fb236fb1eaedb6256ea6758a97b1fbde4a02c
jiunlee19 <tearofglass@naver.com>
2025-11-18 11:07:05 +0900

    feat/TextInput 컴포넌트 props 추가

--- boardgame-frontend/src/app/signup/components/TextInput.tsx
+++ boardgame-frontend/src/app/signup/components/TextInput.tsx
@@ -1,14 +1,15 @@
 interface TextInputProps {
-  title: string;
   name: string;
+  label?: string;
   placeholder?: string;
+  isHidden?: boolean;
 }
-export default function TextInput({ title,name, placeholder }: TextInputProps){
+export default function TextInput({ name, label, placeholder, isHidden }: TextInputProps){
   return (
 
-      <div className="my-[40px] text-[14px]">
-        <label htmlFor={name}>{title}</label>
-        <input name={name} id={name} className=" w-full mt-[12px] p-3 border rounded-xl border-[#161616]" placeholder={`${title}을 입력해주세요${placeholder || ''}`} type="text" maxLength={10} />
+      <div className="mt-[40px] text-sm">
+        <label htmlFor={name} className={isHidden ? 'sr-only' :''}>{label}</label>
+        <input name={name} id={name} className=" w-full mt-[12px] p-3 border rounded-xl border-[#161616] outline-none" placeholder={placeholder} type="text" maxLength={10} />
       </div>
   )
 }
