5d22acd4622b403b5196c4a6ffcb45efec0a164c
MinKyeongHyeon <28599@naver.com>
2025-11-24 23:09:58 +0900

    feat/CardList작성, 더미 데이터 작성

--- boardgame-frontend/src/components/common/CardList.tsx
+++ boardgame-frontend/src/components/common/CardList.tsx
@@ -1,10 +1,40 @@
 import Card from "./Card"
 
 export default function CardList (){
+  const dummyPost = [{
+    id:"a1er1z1",
+    location:"서울시 종로구",
+    title : "스플렌더 초고수만",
+    currentMember: 3,
+    maxMember:5,
+    games:[
+      "루미큐브","스플렌더","젠가"
+    ]
+  },{
+    id:"a1er1z1",
+    location:"서울시 종로구",
+    title : "스플렌더 초고수만",
+    currentMember: 3,
+    maxMember:5,
+    games:[
+      "루미큐브","스플렌더","젠가"
+    ]
+  },{
+    id:"a1er1z1",
+    location:"서울시 종로구",
+    title : "스플렌더 초고수만",
+    currentMember: 3,
+    maxMember:5,
+    games:[
+      "루미큐브","스플렌더","젠가"
+    ]
+  }]
   return (
     <div>
-      <ul>
-        <Card/>
+      <ul className="flex flex-col gap-0.5">
+        {dummyPost.map((post,index)=>(
+        <Card key={index} id={post.id} location={post.location} title={post.title} currentMember={post.currentMember} maxMember={post.maxMember} games={post.games}/>
+        ))}
       </ul>
     </div>
   )
