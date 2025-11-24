e93111014469f7e69a3c51f49dafd29cb7e88ebc
MinKyeongHyeon <28599@naver.com>
2025-11-25 08:27:22 +0900

    feat/확장가능성을 고려해 객체로 반환

--- boardgame-frontend/src/util/dateFormatter.tsx
+++ boardgame-frontend/src/util/dateFormatter.tsx
@@ -1,13 +1,21 @@
-export default function dateFormatter (isoDate:string):string {
+interface DateFormat {
+  month: string;
+  day: string;
+  weekday: string;
+  hours: string;
+  minutes: string;
+}
+
+export default function dateFormatter(isoDate: string): DateFormat {
   const date = new Date(isoDate);
-  
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
-  const hours = String(date.getHours()).padStart(2, '0');
-  const minutes = String(date.getMinutes()).padStart(2, '0');
-  
-  const weekdays = ['일', '월', '화', '수', '목', '금', '토'];
+
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+
+  const weekdays = ["일", "월", "화", "수", "목", "금", "토"];
   const weekday = weekdays[date.getDay()];
-  
-  return `${month}/${day}(${weekday}) ${hours}:${minutes}`;
+
+  return { month, day, hours, minutes, weekday };
 }
