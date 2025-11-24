0e1c8c12b3e4704631b9314637a9e34b0eddff6c
MinKyeongHyeon <28599@naver.com>
2025-11-24 23:09:22 +0900

    feat/Card컴포넌트 작성

--- boardgame-frontend/src/components/common/Card.tsx
+++ boardgame-frontend/src/components/common/Card.tsx
@@ -1,5 +1,47 @@
-export default function Card (){
-  return(
-    <div>Card</div>
-  )
+import Link from "next/link";
+import Image from "next/image";
+import mapPin from "../../../public/icons/map-pin.svg"
+import Badge from "./Badge";
+
+
+interface CardProps {
+  id : string;
+  location : string;
+  title : string;
+  currentMember : number;
+  maxMember : number;
+  games : string [];
+}
+
+
+/**
+ * 
+ * @param id sting 게시글 id
+ * @param location string 주소(서울 종로구)
+ * @param title string 게시글제목
+ * @param currentMember number 지금 참여한 인원수
+ * @param maxMember number 모집하는 총 인원수
+ * @param games string[] 게임태그
+ */
+export default function Card({id, location, title, currentMember,maxMember,games}:CardProps) {
+  const posting = "/"
+  const postingId = id
+  return (
+    <article className="w-[335px] h-[116px] rounded-2xl p-4 bg-white">
+      <Link href={posting} className="flex flex-col gap-2">
+      <div className="flex">
+        <Image src={mapPin} alt="위치" width={18} height={18} />
+        <span className="font-medium text-[13px] text-[#767676]">{location}</span>
+      </div>
+      <h2 className="font-medium text-[15px] text-[#161616]">{title}</h2>
+      <ul className="flex gap-1">
+        <Badge>{`${currentMember}/${maxMember} 명`}</Badge>
+        {games.map((game, index) => (
+          <Badge key={index}>{game}</Badge>
+        ))}
+      </ul>
+      </Link>
+      
+    </article>
+  );
 }
