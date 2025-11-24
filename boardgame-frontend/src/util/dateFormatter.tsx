892f764f4f55bf6d29a513fa4b6e586d9c700a7d
MinKyeongHyeon <28599@naver.com>
2025-11-24 23:32:56 +0900

    feat/날짜 변환 유틸 작성

---
+++ boardgame-frontend/src/util/dateFormatter.tsx
@@ -0,0 +1,13 @@
+export default function dateFormatter (isoDate:string):string {
+  const date = new Date(isoDate);
+  
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  
+  const weekdays = ['일', '월', '화', '수', '목', '금', '토'];
+  const weekday = weekdays[date.getDay()];
+  
+  return `${month}/${day}(${weekday}) ${hours}:${minutes}`;
+}
