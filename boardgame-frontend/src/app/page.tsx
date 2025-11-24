d0bbe8fa82f360632475767ffa59def26f650085
MinKyeongHyeon <28599@naver.com>
2025-11-24 23:11:18 +0900

    fix/스타일 태그 수정  100vh => screen

--- boardgame-frontend/src/app/page.tsx
+++ boardgame-frontend/src/app/page.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center h-[100vh] bg-[#F5F6FA]">
+    <div className="flex flex-col items-center h-screen bg-[#F5F6FA]">
         
 
           <Header/>
