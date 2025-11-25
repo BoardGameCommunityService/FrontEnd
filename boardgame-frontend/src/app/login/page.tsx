cbbe8b104c1a4921f732ae947943f306b71b8f2e
이지언 <tearofglass@naver.com>
2025-11-25 10:16:08 +0900

    chore/기존 /login/page.tsx 리셋

--- boardgame-frontend/src/app/login/page.tsx
+++ boardgame-frontend/src/app/login/page.tsx
@@ -1,5 +1,6 @@
-import type { Metadata } from "next";
 import Image from "next/image";
+import React from "react";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "소셜 로그인",
