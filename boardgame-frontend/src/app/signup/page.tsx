ccd4de10ebf9eaf1d35e292599c4eaa4c16e4dfa
gimeast <slasnrndu@gmail.com>
2025-11-25 14:31:19 +0900

    refactor: Signup페이지 async 제거

--- boardgame-frontend/src/app/signup/page.tsx
+++ boardgame-frontend/src/app/signup/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import SignupForm from "@/components/signup/SignupForm";
 
-export default async function Signup() {
+export default function Signup() {
   return (
     <>
       <Link className="w-fit" href="/login">
