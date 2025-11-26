701303aaa3fa3b806a027030103957191fdf1cd8
jiunlee19 <tearofglass@naver.com>
2025-11-26 23:47:34 +0900

    feat/오늘로부터 한달 동안의 날짜 보여주기, 날짜 레이아웃 마크업

--- boardgame-frontend/src/components/common/Calendar.tsx
+++ boardgame-frontend/src/components/common/Calendar.tsx
@@ -1,7 +1,35 @@
-export default function Calendar (){
+export default function Calendar() {
+  const today = new Date();
+  const week = ["일", "월", "화", "수", "목", "금", "토"];
+
+  // 오늘부터 30일간의 날짜 배열 생성
+  const getDatesForMonth = () => {
+    const dates = [];
+    for (let i = 0; i < 30; i++) {
+      const date = new Date(today);
+      date.setDate(today.getDate() + i);
+      dates.push(date);
+    }
+    return dates;
+  };
+
+  const dates = getDatesForMonth();
+
   return (
-    <div>
-      Calendar
-    </div>
-  )
+    <ul className="w-full overflow-x-scroll [&::-webkit-scrollbar]:hidden flex my-4">
+      {dates.map((date, index) => {
+        const dateNum = date.getDate();
+        const dayName = week[date.getDay()];
+
+        return (
+          <li key={index}>
+            <button className="flex flex-col w-[42px] h-[56px] justify-center">
+              <span>{dateNum}</span>
+              <span>{dayName}</span>
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
