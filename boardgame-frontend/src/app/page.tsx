891657ff7d7ea19550ad60df980389c8e1cf15d4
jiunlee19 <tearofglass@naver.com>
2025-11-26 23:46:26 +0900

    refactor/ 메인페이지 레이아웃 items-center 삭제, 좌우 패딩값 20px 추가

--- boardgame-frontend/src/app/page.tsx
+++ boardgame-frontend/src/app/page.tsx
@@ -1,28 +1,25 @@
-import Header from "@/components/common/Header";
 import Banner from "@/components/common/Banner";
 import Calendar from "@/components/common/Calendar";
 import CardList from "@/components/common/CardList";
+import Header from "@/components/common/Header";
 import Link from "next/link";
-
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center h-screen bg-[#F5F6FA]">
-        
-
-          <Header/>
-          <main>
-            <section>
-              <Banner/>
-            </section>
-            <section>
-              <Calendar/>
-            </section>
-            <section>
-              <CardList/>
-            </section>
-            <Link href="/board/new">+ 모임만들기</Link>
-          </main>
+    <div className="flex flex-col h-screen bg-[#F5F6FA] px-[20px]">
+      <Header />
+      <main>
+        <section>
+          <Banner />
+        </section>
+        <section>
+          <Calendar />
+        </section>
+        <section>
+          <CardList />
+        </section>
+        <Link href="/board/new">+ 모임만들기</Link>
+      </main>
     </div>
   );
 }
