69a31f201e8ab751aba48e5bf3aa645fa17dff16
gimeast <slasnrndu@gmail.com>
2025-12-02 18:23:22 +0900

    feat: 게임 선택 BottomSheet - 검색, 데이터, 하단 버튼 퍼블리싱

--- boardgame-frontend/src/components/bottom-sheet/GameSelect.tsx
+++ boardgame-frontend/src/components/bottom-sheet/GameSelect.tsx
@@ -2,12 +2,16 @@
 
 import Image from "next/image";
 import useBottomSheetStore from "@/stores/useBottomSheetStore";
+import { BoardGames } from "@/content/games/games";
+import Button from "@/components/common/Button";
 
 export default function GameSelect() {
   const { setClose } = useBottomSheetStore();
+
   return (
-    <div>
-      <div className="flex gap-3">
+    <div className="flex flex-col h-full">
+      {/* 상단 고정: 검색 */}
+      <div className="flex gap-3 pb-4">
         <div className="flex w-full relative">
           <Image
             className="absolute left-3 top-1/2 -translate-y-1/2"
@@ -26,9 +30,32 @@ export default function GameSelect() {
             placeholder="게임을 검색해주세요."
           />
         </div>
-        <button aria-label="닫기" onClick={setClose}>
+        <button className="cursor-pointer" aria-label="닫기" onClick={setClose}>
           <Image src="/icons/ic_close.svg" alt="" width={24} height={24} />
         </button>
+      </div>
+
+      {/* 중간 스크롤: 게임 리스트 */}
+      <div className="flex-1 overflow-y-auto scrollbar-hide">
+        <ul className="flex flex-col gap-3 ">
+          {BoardGames.map((k: { initial: string; games: Array<string> }) => (
+            <li key={k.initial} className="text-[#767676] text-[13px] leading-5">
+              <p className="border-b border-[#F1F1F4]">{k.initial}</p>
+              <ul className="mt-3 flex gap-2 flex-wrap">
+                {k.games.map((game) => (
+                  <li key={k.initial + game} className="border border-[#DEE1E6] rounded-[34px]">
+                    <button className="py-2 px-2.5 w-full cursor-pointer">{game}</button>
+                  </li>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      {/* 하단 고정: 버튼 */}
+      <div className="mt-3 pt-4 border-t border-[#F1F1F4] bg-white">
+        <Button type="button" text="선택 완료" btnSize="medium" />
       </div>
     </div>
   );
