945cf1e754cf6c741b7e682972e11ae7d5b45999
gimeast <slasnrndu@gmail.com>
2025-12-05 20:24:06 +0900

    fix: SignupLocationForm 컴포넌트 수정

--- boardgame-frontend/src/components/signup/SignupLocationForm.tsx
+++ boardgame-frontend/src/components/signup/SignupLocationForm.tsx
@@ -40,35 +40,40 @@ export default function SignupLocationForm() {
 
   return (
     <>
-      <Link href="/signup">
-        <Image src="/icons/ic_back.svg" alt="뒤로가기 버튼" width={24} height={24} />
-      </Link>
+      <header>
+        <Link href="/signup">
+          <Image src="/icons/ic_back.svg" alt="뒤로가기 버튼" width={24} height={24} />
+        </Link>
+      </header>
 
-      <div className="flex flex-col">
-        <h2 className="mt-4 mb-[40px]  font-semibold text-2xl text-[#161616]">활동 지역을 선택해주세요</h2>
+      <main>
+        <section className="flex flex-col gap-3">
+          <h2 className="mt-4 font-semibold text-2xl text-[#161616]">활동 지역을 선택해주세요</h2>
 
-        <TextInput
-          label="활동지역"
-          name="userLocation"
-          placeholder="지역구를 입력해주세요.(ex.강남구, 서초구)"
-          isHidden={true}
-          value={locationInput}
-          onChange={handleSearchChange}
-        />
+          <div className="mt-7">
+            <TextInput
+              label="활동지역"
+              name="userLocation"
+              placeholder="지역구를 입력해주세요.(ex.강남구, 서초구)"
+              isHidden={true}
+              value={locationInput}
+              onChange={handleSearchChange}
+            />
+          </div>
+          <Button
+            type="button"
+            text={"현재 위치로 찾기"}
+            btnSize="medium"
+            bgColor="bg-[#06E393]"
+            icon={<Image src="/icons/ic_gps.svg" alt="버튼" width={20} height={20} />}
+            textColor="text-black"
+            onClick={handleCurrentLocationClick}
+            disabled={isLoading}
+          />
+        </section>
 
-        <Button
-          type="button"
-          text={"현재 위치로 찾기"}
-          btnSize="medium"
-          bgColor="bg-[#06E393]"
-          icon={<Image src="/icons/ic_gps.svg" alt="버튼" width={20} height={20} />}
-          textColor="text-black"
-          onClick={handleCurrentLocationClick}
-          disabled={isLoading}
-        />
-      </div>
-
-      <LocationSearchResults results={searchResults} onSelect={handleLocationSelect} isLoading={isLoading} />
+        <LocationSearchResults results={searchResults} onSelect={handleLocationSelect} isLoading={isLoading} />
+      </main>
     </>
   );
 }
