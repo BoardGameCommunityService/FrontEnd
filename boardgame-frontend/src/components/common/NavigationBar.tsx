23493337823002fafc49b585e110a6acbb9b2839
gimeast <slasnrndu@gmail.com>
2025-12-05 15:38:58 +0900

    fix: 마이페이지 헤더 뒤로가기 링크 수정

--- boardgame-frontend/src/components/common/NavigationBar.tsx
+++ boardgame-frontend/src/components/common/NavigationBar.tsx
@@ -6,7 +6,7 @@ export default function NavigationBar({ title, elements }: { title: string; elem
   return (
     <header className="px-5 flex justify-between items-center h-12">
       <div className="flex items-center gap-0.5">
-        <Link href="/public" className="cursor-pointer">
+        <Link href="/" className="cursor-pointer">
           <Image src="/icons/ic_back.svg" alt="뒤로가기" width={24} height={24} />
         </Link>
         <h1 className="text-[#161616] font-bold text-[20px] leading-7">{title}</h1>
