2672083160772b763a176f3c73e081ff26a50dc0
gimeast <slasnrndu@gmail.com>
2025-12-05 16:02:52 +0900

    fix: 마이페이지 레이아웃 수정

--- boardgame-frontend/src/app/mypage/layout.tsx
+++ boardgame-frontend/src/app/mypage/layout.tsx
@@ -5,5 +5,5 @@ export default function Layout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div className="bg-[#F5F6FA] pb-[234px]">{children}</div>;
+  return <div className="bg-[#F5F6FA] min-h-screen">{children}</div>;
 }
