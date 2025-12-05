1ba59f69ad648e06065dd92cdf7e4bc2a9e9602f
gimeast <slasnrndu@gmail.com>
2025-12-05 14:22:52 +0900

    feat: 마이페이지 프로필 섹션 추가

---
+++ boardgame-frontend/src/app/mypage/page.tsx
@@ -0,0 +1,63 @@
+import Link from "next/link";
+import Image from "next/image";
+import NavigationBar from "@/components/common/NavigationBar";
+import React from "react";
+
+export default function Page() {
+  return (
+    <>
+      <NavigationBar
+        title="마이페이지"
+        elements={
+          <button className="cursor-pointer relative">
+            <Image src="/icons/ic_alim.svg" alt="공유하기 버튼" width={36} height={36} />
+            <span className="rounded-[50%] bg-[#FC3B45] w-1.5 h-1.5 inline-block absolute top-1.5 right-1.5"></span>
+          </button>
+        }
+      />
+
+      <main className="px-5">
+        <section className="bg-[#FFFFFF] rounded-2xl p-5">
+          <h2 className="sr-only">프로필</h2>
+          <Link className="flex justify-between items-center cursor-pointer" href="/mypage/profile">
+            <div className="flex items-center gap-3 text-lg leading-[26px] font-bold">
+              <Image
+                className={"w-12 h-12"}
+                src="/temp_profile.svg"
+                alt="프로필 이미지"
+                width={48}
+                height={48}
+                loading="eager"
+              />
+              <span>즐겜러</span>
+            </div>
+            <button>
+              <Image className="w-5 h-5" src="/icons/ic_right20.svg" alt="" width={20} height={20} />
+            </button>
+          </Link>
+          <ul className="mt-4 flex w-full bg-[#F5F6FA] rounded-xl">
+            <li className="flex-1 py-3">
+              <Link className="flex flex-col items-center" href="/mypage/meeting/created">
+                <span className="text-[#767676] text-xs leading-[18px]">만든모임</span>
+                <span className="text-[#121212] text-xl leading-7 font-medium">2</span>
+              </Link>
+            </li>
+            <li className="flex-1 py-3">
+              <Link className="flex flex-col items-center" href="/mypage/meeting/joined">
+                <span className="text-[#767676] text-xs leading-[18px]">참여한모임</span>
+                <span className="text-[#121212] text-xl leading-7 font-medium">23</span>
+              </Link>
+            </li>
+            <li className="flex-1 py-3">
+              <Link className="flex flex-col items-center" href="/mypage/meeting/waiting">
+                <span className="text-[#767676] text-xs leading-[18px]">신청 대기중</span>
+                <span className="text-[#121212] text-xl leading-7 font-medium">2</span>
+              </Link>
+            </li>
+          </ul>
+        </section>
+        <section></section>
+      </main>
+    </>
+  );
+}
