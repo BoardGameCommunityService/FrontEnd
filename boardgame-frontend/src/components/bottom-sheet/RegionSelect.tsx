d4ceeac3aae1214fe9b984f8b9bcbc59097ef17e
jiunlee19 <tearofglass@naver.com>
2025-12-08 16:00:29 +0900

    feat/ RegionSelect 컴포넌트 검색창 마크업 및 스타일링

--- boardgame-frontend/src/components/bottom-sheet/RegionSelect.tsx
+++ boardgame-frontend/src/components/bottom-sheet/RegionSelect.tsx
@@ -1,10 +1,11 @@
 import { useDebounce } from "@/hooks/useDebounce";
 import { useRegion } from "@/hooks/useRegion";
+import Image from "next/image";
 import { useEffect, useState } from "react";
 
 export default function RegionSelect() {
   const [keyword, setKeyword] = useState("");
-  const debouncedSearch = useDebounce(keyword, 300);
+  const debouncedSearch = useDebounce(keyword, 500);
   const { places, isLoading, error, searchPlaces, totalCount } = useRegion();
 
   useEffect(() => {
@@ -19,9 +20,23 @@ export default function RegionSelect() {
 
   return (
     <>
-      <form action="#">
-        <label htmlFor="keywordSearch"></label>
-        <input id="keywordSearch" type="text" onChange={handleSearch} value={keyword} />
+      <form action="#" className="flex rounded gap-3">
+        <div className="flex flex-1 bg-[#F5F6FA] px-3 py-[10px] gap-2 rounded-lg">
+          <Image src="/icons/ic_search_gray.svg" width={16} height={16} alt="검색" />
+          <label htmlFor="keywordSearch" className="sr-only">
+            모임 장소 검색
+          </label>
+          <input
+            id="keywordSearch"
+            type="text"
+            onChange={handleSearch}
+            value={keyword}
+            className="outline-none text-[14px] flex-1"
+          />
+        </div>
+        <button type="button" onClick={() => setKeyword("")}>
+          <Image src="/icons/ic_close_gray.svg" width={24} height={24} alt="검색창 초기화"></Image>
+        </button>
       </form>
     </>
   );
