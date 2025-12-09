ef769e5baaa44dc931d9af4527fb8e9c5c17be91
MinKyeongHyeon <28599@naver.com>
2025-12-09 10:44:12 +0900

    fix/필드네임 수정

--- boardgame-frontend/src/components/common/CardList.tsx
+++ boardgame-frontend/src/components/common/CardList.tsx
@@ -27,7 +27,7 @@ export default function CardList({ results }: CardListProps) {
             currentMember={post.currentParticipants}
             maxMember={post.maxParticipants}
             meetingDate={post.meetingAt}
-            games={parseTags(post.tagsJson)}
+            games={parseTags(post.gameNameJson)}
           />
         ))}
       </ul>
