50a922082a4c4c3ab53900fd06303f4180db9a92
jiunlee19 <tearofglass@naver.com>
2025-12-09 09:42:57 +0900

    fix/ form 제거, button type 설정

--- boardgame-frontend/src/components/bottom-sheet/RegionSelect.tsx
+++ boardgame-frontend/src/components/bottom-sheet/RegionSelect.tsx
@@ -24,7 +24,7 @@ export default function RegionSelect() {
 
   return (
     <div className="h-full flex flex-col">
-      <form action="#" className="flex rounded gap-3">
+      <div className="flex rounded gap-3">
         <div className="flex flex-1 bg-[#F5F6FA] px-3 py-[10px] gap-2 rounded-lg">
           <Image src="/icons/ic_search_gray.svg" width={16} height={16} alt="검색" />
           <label htmlFor="keywordSearch" className="sr-only">
@@ -36,26 +36,26 @@ export default function RegionSelect() {
             onChange={handleSearch}
             value={keyword}
             placeholder="모임 장소를 검색해주세요."
-            className="outline-none text-[14px] flex-1"
+            className="outline-none text-sm flex-1"
           />
         </div>
-        <button className="cursor-pointer" aria-label="닫기" onClick={setClose}>
+        <button type="button" className="cursor-pointer" aria-label="닫기" onClick={setClose}>
           <Image src="/icons/ic_close_gray.svg" width={24} height={24} alt="검색창 초기화"></Image>
         </button>
-      </form>
+      </div>
 
       {!isLoading && !error && places.length > 0 && (
-        <ul className="flex flex-1 flex-col gap-4 mt-4 pb-6 h-full overflow-y-scroll scrollbar-hide">
+        <ul className="flex flex-1 flex-col gap-4 my-4 h-full overflow-y-scroll scrollbar-hide">
           {places.map((place) => (
             <li
               key={place.id}
               onClick={handleMeetingRegion}
-              className="pb-4 border-b border-[#E3E5E9] flex gap-[5px] items-start cursor-pointer"
+              className="border-[#E3E5E9] flex gap-[5px] items-start cursor-pointer"
             >
               <Image src="/icons/ic_marker_outline.svg" width={16} height={16} alt="" className="mt-[3px]"></Image>
               <div className="flex-col">
-                <h4 className="text-base font-semibold text-[16px] text-[#161616]">{place.place_name}</h4>
-                <p className="mt-1 text-sm font-normal text-[14px] text-[#767676]">{place.address_name}</p>
+                <h4 className="text-base font-semibold text-[#161616]">{place.place_name}</h4>
+                <p className="mt-1 text-sm font-normal text-[#767676]">{place.address_name}</p>
               </div>
             </li>
           ))}
