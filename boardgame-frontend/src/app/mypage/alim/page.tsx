41b369330e1da89d05b2e3c640837b86882efb84
gimeast <slasnrndu@gmail.com>
2025-12-14 08:24:02 +0900

    feat: 알림 목록 조회 api 연결

--- boardgame-frontend/src/app/mypage/alim/page.tsx
+++ boardgame-frontend/src/app/mypage/alim/page.tsx
@@ -2,19 +2,46 @@ import NavigationBar from "@/components/common/NavigationBar";
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { auth } from "@/auth";
 
-export default function Page() {
-  const dummy = [
-    { id: 1, type: "system", title: "강남구에 새로운 모임이 개설되었어요!", content: "개설된 모임을 확인해보세요" },
-    { id: 2, type: "system", title: "문의한 글에 답변이 도착했어요.", content: "답변을 확인해보세요." },
-    {
-      id: 3,
-      type: "invite",
-      title: "‘한판만'님이 모임 참가 신청을 보냈어요.",
-      content: "보드메이트와 함께 게임을 즐겨보세요.",
-    },
-    { id: 4, type: "invite", title: "모임 신청이 반려되었어요.", content: "아쉽지만 다른 모임에 참가해보세요." },
-  ];
+interface NotificationItem {
+  createdAt: string;
+  id: number;
+  isRead: boolean;
+  message: string;
+  readAt: string | null;
+  resourceId: number;
+  title: string;
+  type: "REGION_MEETING" | "MEETING_APPLICATION" | "APPLICATION_APPROVED" | "APPLICATION_DENIED";
+}
+
+interface Notification {
+  items: Array<NotificationItem>;
+  total: number;
+}
+
+const getData = async () => {
+  const session = await auth();
+
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_HOST}/api/notifications?page=0&size=15`, {
+      headers: {
+        Authorization: `Bearer ${session?.user?.accessToken}`,
+      },
+    });
+
+    if (!res.ok) throw new Error();
+
+    const result: Notification = await res.json();
+    return result ?? [];
+  } catch (error) {
+    console.error("알림 목록조회 api 호출: ", error);
+    return { items: [], total: 0 };
+  }
+};
+
+export default async function Page() {
+  const { items: notifications, total } = await getData();
 
   return (
     <>
@@ -23,18 +50,18 @@ export default function Page() {
         <section className="mt-2">
           <h2 className="sr-only">알림 목록</h2>
           <ul className="px-5 flex flex-col gap-1">
-            {dummy.map((data) => (
-              <li key={data.id} className="flex gap-3 p-4 bg-white rounded-xl cursor-pointer">
-                <Link href="#">
+            {notifications.map((data) => (
+              <li key={data.id} className="bg-white rounded-xl p-4 cursor-pointer">
+                <Link href="#" className="flex gap-3">
                   <Image
-                    src={`${data.type === "system" ? "/icons/ic_logo_black.svg" : "/icons/ic_logo_green.svg"}`}
+                    src={`${data.type === "REGION_MEETING" ? "/icons/ic_logo_black.svg" : "/icons/ic_logo_green.svg"}`}
                     alt=""
                     width={36}
                     height={36}
                   />
                   <div className="text-[14px] leading-[22px]">
                     <h3 className="font-semibold text-[#363636]">{data.title}</h3>
-                    <p>{data.content}</p>
+                    <p>{data.message}</p>
                   </div>
                 </Link>
               </li>
