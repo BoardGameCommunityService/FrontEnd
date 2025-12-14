307e9ebe12ef57fbcc61ebb0e30e3625e487127b
gimeast <slasnrndu@gmail.com>
2025-12-14 11:19:39 +0900

    feat: 알림 목록 조회 페이징 중간 커밋

--- boardgame-frontend/src/app/mypage/alim/page.tsx
+++ boardgame-frontend/src/app/mypage/alim/page.tsx
@@ -1,30 +1,14 @@
 import NavigationBar from "@/components/common/NavigationBar";
 import React from "react";
-import Image from "next/image";
-import Link from "next/link";
 import { auth } from "@/auth";
-
-interface NotificationItem {
-  createdAt: string;
-  id: number;
-  isRead: boolean;
-  message: string;
-  readAt: string | null;
-  resourceId: number;
-  title: string;
-  type: "REGION_MEETING" | "MEETING_APPLICATION" | "APPLICATION_APPROVED" | "APPLICATION_DENIED";
-}
-
-interface Notification {
-  items: Array<NotificationItem>;
-  total: number;
-}
+import Notifications from "@/components/board/alim/Notifications";
+import { Notification } from "@/types/Notification";
 
 const getData = async () => {
   const session = await auth();
 
   try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_HOST}/api/notifications?page=0&size=15`, {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_HOST}/api/notifications?page=0&size=10`, {
       headers: {
         Authorization: `Bearer ${session?.user?.accessToken}`,
       },
@@ -35,13 +19,13 @@ const getData = async () => {
     const result: Notification = await res.json();
     return result ?? [];
   } catch (error) {
-    console.error("알림 목록조회 api 호출: ", error);
+    console.error("알림 목록조회 api 에러(서버 컴포넌트): ", error);
     return { items: [], total: 0 };
   }
 };
 
 export default async function Page() {
-  const { items: notifications, total } = await getData();
+  const initialData: Notification = await getData();
 
   return (
     <>
@@ -49,24 +33,7 @@ export default async function Page() {
       <main>
         <section className="mt-2">
           <h2 className="sr-only">알림 목록</h2>
-          <ul className="px-5 flex flex-col gap-1">
-            {notifications.map((data) => (
-              <li key={data.id} className="bg-white rounded-xl p-4 cursor-pointer">
-                <Link href="#" className="flex gap-3">
-                  <Image
-                    src={`${data.type === "REGION_MEETING" ? "/icons/ic_logo_black.svg" : "/icons/ic_logo_green.svg"}`}
-                    alt=""
-                    width={36}
-                    height={36}
-                  />
-                  <div className="text-[14px] leading-[22px]">
-                    <h3 className="font-semibold text-[#363636]">{data.title}</h3>
-                    <p>{data.message}</p>
-                  </div>
-                </Link>
-              </li>
-            ))}
-          </ul>
+          <Notifications initialData={initialData} />
         </section>
       </main>
     </>
---
+++ boardgame-frontend/src/components/board/alim/Notifications.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { Notification, NotificationItem } from "@/types/Notification";
+import { useInView } from "react-intersection-observer";
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+
+export default function Notifications({ initialData }: { initialData: Notification }) {
+  const { data: session, status } = useSession();
+
+  const [notifications, setNotifications] = useState<NotificationItem[]>(initialData.items);
+  const [page, setPage] = useState(0);
+  const [hasMore, setHasMore] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { ref, inView } = useInView({
+    threshold: 0,
+    onChange: async (inView) => {
+      if (inView && hasMore && !isLoading) {
+        await loadMore();
+      }
+    },
+  });
+
+  async function loadMore() {
+    if (status === "loading") return;
+
+    try {
+      const nextPage = page + 1;
+
+      setIsLoading(true);
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_HOST}/api/notifications?page=${nextPage}&size=10`, {
+        headers: {
+          Authorization: `Bearer ${session?.user?.accessToken}`,
+        },
+      });
+
+      if (!res.ok) throw new Error();
+
+      const result: Notification = await res.json();
+      console.log(result);
+      setNotifications((prev) => [...prev, ...result.items]);
+      setPage((prev) => prev + 1);
+
+      return result ?? [];
+    } catch (error) {
+      console.error("알림 목록조회 api 에러(클라이언트 컴포넌트): ", error);
+      return { items: [], total: 0 };
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <>
+      <ul className="px-5 flex flex-col gap-1">
+        {notifications.map((data) => (
+          <li key={data.id || data.resourceId} className="bg-white rounded-xl p-4 cursor-pointer">
+            <Link href="#" className="flex gap-3">
+              <Image
+                src={`${data.type === "REGION_MEETING" ? "/icons/ic_logo_black.svg" : "/icons/ic_logo_green.svg"}`}
+                alt=""
+                width={36}
+                height={36}
+              />
+              <div className="text-[14px] leading-[22px]">
+                <h3 className="font-semibold text-[#363636]">{data.title}</h3>
+                <p>{data.message}</p>
+              </div>
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <div ref={ref}></div>
+    </>
+  );
+}
