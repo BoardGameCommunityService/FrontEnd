1a8cc811dd1b081bd93df02fd0475fea3eb49c9a
jiunlee19 <tearofglass@naver.com>
2025-12-16 14:02:26 +0900

    design/ 텍스트 색깔 수정

--- boardgame-frontend/src/app/mypage/inquiries/list/page.tsx
+++ boardgame-frontend/src/app/mypage/inquiries/list/page.tsx
@@ -45,7 +45,7 @@ export default function InquiriesListPage() {
         {inquiries.length === 0 && (
           <section className="flex flex-col items-center justify-center h-[calc(100%-100px)]">
             <Image src="/icons/ic_empty_logo_with_bubble.svg" width={45} height={58} alt=""></Image>
-            <p className="mt-6 font-medium text-base text-center text-[#9B9B9B]">문의 내역이 없습니다.</p>
+            <p className="mt-6 font-medium text-base text-center text-[#363636]">문의 내역이 없습니다.</p>
           </section>
         )}
         {/* 문의 내역 */}
