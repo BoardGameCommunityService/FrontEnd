6b450c39884116585bde78daccc0144fbc0dac79
MinKyeongHyeon <28599@naver.com>
2025-12-16 14:24:15 +0900

    feat/다양한 디자인에 대응할 수 있도록 prop추가
    # Conflicts:
    #	boardgame-frontend/src/components/search/EmptyState.tsx

--- boardgame-frontend/src/components/search/EmptyState.tsx
+++ boardgame-frontend/src/components/search/EmptyState.tsx
@@ -2,20 +2,34 @@ import Image from "next/image";
 import Link from "next/link";
 import emptyLogo from "../../../public/emptyLogo.svg";
 
+type EmptyStateProps = {
+  isBottom?: boolean;
+  textSize?: "sm" | "lg";
+};
+
 //검색 결과 없음 Empty state
-export default function EmptyState() {
+export default function EmptyState({ isBottom, textSize }: EmptyStateProps) {
   return (
     <div className="flex flex-col items-center">
-      <Image src={emptyLogo} alt="보드메이트" width={48.75} height={58.5} className="-rotate-15" />
-      <h2 className="mt-4 font-semibold text-[15px] text-[#363636]">첫 모임을 만들어볼까요?</h2>
-
-      <p className="mt-1 font-normal text-[13px] text-[#767676]">모임을 만들어 보드메이트들을 만나보세요!</p>
-      <Link
-        href="/board/new"
-        className="mt-6 w-20 h-[34px] px-2.5 py-2 rounded-[34px] bg-[#06E393] font-semibold text-[13px] text-[#161616]"
+      <Image src={emptyLogo} alt="보드메이트" width={57.5} height={64} />
+      <h2
+        className={`mt-4 font-semibold ${textSize === "sm" ? "text-[15px] leading-[24px]" : "text-2xl"} text-[#363636]`}
       >
-        {"모임 만들기"}
-      </Link>
+        첫 모임을 만들어볼까요?
+      </h2>
+      <p className={`mt-1 font-normal ${textSize === "sm" ? "text-[13px] leading-[20px]" : "text-xl"} text-[#767676]`}>
+        모임을 만들어 보드메이트들을 만나보세요!
+      </p>
+      {isBottom ? (
+        <Link
+          href="/board/new"
+          className="mt-6 w-20 h-[34px] px-2.5 py-2 rounded-[34px] bg-[#06E393] font-semibold text-[13px] text-[#161616]"
+        >
+          {"모임 만들기"}
+        </Link>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
