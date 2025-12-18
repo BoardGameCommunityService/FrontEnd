276c80fc40377fc0eb3a97afe19c770b26fa7438
jiunlee19 <tearofglass@naver.com>
2025-12-18 11:37:03 +0900

    feat/내 문의 목록 조회 aPI 연결

--- boardgame-frontend/src/app/mypage/inquiries/list/page.tsx
+++ boardgame-frontend/src/app/mypage/inquiries/list/page.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import Inquiry from "@/components/inquiry/Inquiry";
+import { useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 interface Inquiry {
   id: number;
@@ -12,25 +16,65 @@ interface Inquiry {
 
 export default function InquiriesListPage() {
   // 샘플 문의 내역 데이터
-  const inquiries = [
-    {
-      id: 0,
-      title: "문의는 포도가 좋아",
-      content: "문의 테스트 1입니다. 문의 테스트 1입니다.",
-      answer: null,
-      createdAt: "2025.12.06",
-    },
-    {
-      id: 1,
-      title: "문의2",
-      content: "문의 테스트 2입니다. 문의 테스트 2입니다.",
-      answer: "문의 답변입니다. 문의 답변입니다. 문의 답변입니다.",
-      createdAt: "2025.12.16",
-    },
-  ];
+  // const inquiries = [
+  //   {
+  //     id: 0,
+  //     title: "문의는 포도가 좋아",
+  //     content: "문의 테스트 1입니다. 문의 테스트 1입니다.",
+  //     answer: null,
+  //     createdAt: "2025.12.06",
+  //   },
+  //   {
+  //     id: 1,
+  //     title: "문의2",
+  //     content: "문의 테스트 2입니다. 문의 테스트 2입니다.",
+  //     answer: "문의 답변입니다. 문의 답변입니다. 문의 답변입니다.",
+  //     createdAt: "2025.12.16",
+  //   },
+  // ];
 
   // 문의 내역이 없을 때 빈 배열로 설정
   // const inquiries = [] as Inquiry[];
+
+  const [inquiries, setInquiries] = useState<Inquiry[] | undefined>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const { data: session, status } = useSession();
+
+  // 내 문의 목록 API
+  const fetchInquiries = async () => {
+    if (!session?.user?.accessToken) {
+      return;
+    }
+    try {
+      setIsLoading(true);
+
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_HOST}/api/inquiries/my`, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${session.user.accessToken}`,
+        },
+        credentials: "include",
+      });
+      if (!response.ok) {
+        throw new Error(`요청 실패: ${response.status}`);
+      }
+
+      const data = await response.json();
+      return data as Inquiry[];
+    } catch (error) {
+      console.error("데이터를 불러오지 못했습니다:", error);
+      throw error;
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchInquiries()
+      .then((data) => setInquiries(data))
+      .catch((err) => console.error(err));
+    console.log(inquiries);
+  }, []);
 
   return (
     <div className="pt-11 bg-white h-screen flex flex-col">
@@ -42,16 +86,14 @@ export default function InquiriesListPage() {
       </header>
       <main className="flex-1 overflow-y-auto">
         {/* 문의 내역이 없을 때 보여주는 화면 */}
-        {inquiries.length === 0 && (
+        {inquiries?.length === undefined && (
           <section className="flex flex-col items-center justify-center h-[calc(100%-100px)]">
             <Image src="/icons/ic_empty_logo_with_bubble.svg" width={45} height={58} alt=""></Image>
             <p className="mt-6 font-medium text-base text-center text-[#363636]">문의 내역이 없습니다.</p>
           </section>
         )}
         {/* 문의 내역 */}
-        {inquiries.map((inquiry: Inquiry) => (
-          <Inquiry key={inquiry.id} inquiry={inquiry} />
-        ))}
+        {inquiries && inquiries.map((inquiry: Inquiry) => <Inquiry key={inquiry.id} inquiry={inquiry} />)}
       </main>
       <div className="fixed bottom-0 left-0 right-0 px-5 pb-10 pt-[18px] bg-white flex justify-center">
         <Link
