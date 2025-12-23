94438acc735d8d9f999255a668899d4e592c2d47
jiunlee19 <tearofglass@naver.com>
2025-12-23 16:16:00 +0900

    chore/ 삭제한 regionCode 쿼리 재작성

--- boardgame-frontend/src/app/page.tsx
+++ boardgame-frontend/src/app/page.tsx
@@ -43,7 +43,7 @@ export default function Home() {
       const date = `${year}${month}${day}`;
 
       const res = await fetch(
-        `${process.env.NEXT_PUBLIC_API_SERVER_HOST}/api/meetings?page=${pageNum}&size=15&date=${date}`
+        `${process.env.NEXT_PUBLIC_API_SERVER_HOST}/api/meetings?page=${pageNum}&size=15&date=${date}&regionCode=${selectedRegion}`
       );
 
       if (!res.ok) throw new Error("데이터 fetch 에러");
